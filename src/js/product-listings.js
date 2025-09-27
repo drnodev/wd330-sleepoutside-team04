aadab4bc12a0d29622b8c07e82f14c5db78953c0
Boiketlo20 <nzimandeyolanda058@gmail.com>
2025-09-27 22:59:06 +0200

    Merging with main

--- src/js/product-listings.js
+++ src/js/product-listings.js
@@ -1,11 +1,11 @@
-import ProductData from "./ProductData.mjs";
+import ExternalService from "./ExternalServices.mjs";
 import ProductList from "./ProductList.mjs";
 import { getParam, loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
 
-const category    = getParam("category");
-const dataSource  = new ProductData();
+const category = getParam("category");
+const dataSource = new ExternalService();
 const listElement = document.querySelector(".product-list");
 const productList = new ProductList(category, dataSource, listElement);
 
